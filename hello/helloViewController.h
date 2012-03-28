4e6a3a9f154f4a0e6e16ec507a5487e48b656d5e
stefan mueller <hello@smixd.de>
2012-03-28 20:20:23 +0200

    erstellt

--- hello/helloViewController.h
+++ hello/helloViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface helloViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property(nonatomic, readonly, retain) UILabel *titleLabel;
+
+- (IBAction)button:(id)sender;
 
 @end
--- hello/helloViewController.m
+++ hello/helloViewController.m
@@ -13,15 +13,43 @@
 @end
 
 @implementation helloViewController
+@synthesize label;
+@synthesize titleLabel;
+  
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [label setFont:[UIFont fontWithName:@"TitilliumText22L-Bold" size:label.font.pointSize]]; 
+    
+    /*UIButton *button = [UIButton buttonWithType: UIButtonTypeRoundedRect];
+    
+    button.titleLabel.font = [UIFont fontWithName:@"TitilliumText22L-Bold" size:22.0]; 
+    */
 	// Do any additional setup after loading the view, typically from a nib.
+    /*
+    NSArray *familyNames = [[NSArray alloc] initWithArray:[UIFont familyNames]];
+    
+    NSArray *fontNames;
+    NSInteger indFamily, indFont;
+    for (indFamily=0; indFamily<[familyNames count]; ++indFamily)
+    {
+        NSLog(@"Family name: %@", [familyNames objectAtIndex:indFamily]);
+        fontNames = [[NSArray alloc] initWithArray:
+                     [UIFont fontNamesForFamilyName:
+                      [familyNames objectAtIndex:indFamily]]];
+        for (indFont=0; indFont<[fontNames count]; ++indFont)
+        {
+            NSLog(@"    Font name: %@", [fontNames objectAtIndex:indFont]);
+        }
+    }
+     */
 }
 
 - (void)viewDidUnload
 {
+    [self setLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,4 +59,10 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+
+
+- (IBAction)button:(id)sender {
+    
+
+}
 @end
